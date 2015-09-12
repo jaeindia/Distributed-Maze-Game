fee70410537a0f67dcd313d1c0336ee206565ad9
jaeindia <jaeindia@gmail.com>
2015-09-12 18:23:31 +0800

    Constant class - Add

---
+++ src/main/java/Client/Constant.java
@@ -0,0 +1,12 @@
+package Client;
+
+public class Constant {
+	/*
+	 * 
+	 * Maintain all Constant values
+	 * 
+	 */
+	public static final String RMIID = "MazeGame";
+	public static final int RMIPORT = 222;
+
+}

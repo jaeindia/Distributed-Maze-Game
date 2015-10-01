0ae9fcfccf70be73096214e52b9fd190d83f2811
jaeindia <jaeindia@gmail.com>
2015-10-01 14:44:46 +0800

    move

---
+++ src/main/java/Peer2Peer/Constant.java
@@ -0,0 +1,12 @@
+package Peer2Peer;
+
+public class Constant {
+	/*
+	 * 
+	 * Maintain all Constant values
+	 * 
+	 */
+	public static final String RMIID = "MazeGameP2P";
+	public static final int RMIPORT = 224;
+
+}
---
+++ src/main/java/Peer2Peer/Coordinate.java
@@ -0,0 +1,78 @@
+package Peer2Peer;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class Coordinate implements Serializable {
+
+	@Override
+	public String toString() {
+		return "Coordinate [row=" + row + ", column=" + column + "]";
+	}
+
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3205049466478759683L;
+
+	public  int row;
+	public  int column;
+	
+	
+	public Coordinate(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
+	
+	
+	public int getRow() {
+		return row;
+	}
+
+
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+
+
+	public int getColumn() {
+		return column;
+	}
+
+
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+
+	@Override
+    public int hashCode() {
+        return new HashCodeBuilder(11, 37). // 2 randomly chosen prime numbers
+            // if deriving: appendSuper(super.hashCode()).
+            append(this.getRow()).
+            append(this.getColumn()).
+            toHashCode();
+    }	
+
+	@Override
+	public boolean equals(Object o){	
+	  Coordinate obj = (Coordinate) o;
+		boolean equal = false;
+//		System.out.println(obj.row +"-"+ this.row+","+obj.column+"-"+this.column);
+//		System.out.println(obj.getRow() +"-"+ this.getRow()+","+obj.getColumn()+"-"+this.getColumn());
+		if(obj.getRow()==this.getRow() && obj.getColumn()==this.getColumn()){
+			equal = true;
+		}
+		return equal;
+		
+	}
+
+}
